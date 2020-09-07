17a1c066ae3b361268ded4b770ea63efc5f12abd
Mike van Viegen <Mike.van.Viegen@rabobank.nl>
2020-09-07 11:15:21 +0200

    Initial maven project setup

---
+++ fizzbuzz/src/test/java/nl/rabobank/tryout/fizzbuzz/FizzbuzzTest.java
@@ -0,0 +1,18 @@
+package nl.rabobank.tryout.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class FizzbuzzTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assert( true );
+    }
+}
---
+++ mastermind/src/test/java/nl/rabobank/tryout/mastermind/MastermindTest.java
@@ -0,0 +1,18 @@
+package nl.rabobank.tryout.mastermind;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MastermindTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assert( true );
+    }
+}
