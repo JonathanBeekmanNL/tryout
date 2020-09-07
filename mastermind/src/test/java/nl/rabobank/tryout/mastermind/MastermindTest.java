56cac991e1ad3adad7b5022f887e74db4ee358b1
Mike van Viegen <Mike.van.Viegen@rabobank.nl>
2020-09-07 11:32:55 +0200

    Setup first failing test

--- mastermind/src/test/java/nl/rabobank/tryout/mastermind/MastermindTest.java
+++ mastermind/src/test/java/nl/rabobank/tryout/mastermind/MastermindTest.java
@@ -2,11 +2,20 @@ package nl.rabobank.tryout.mastermind;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MastermindTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertEquals(false, true);
+    public void should_return_misplaced_pegs_and_correct_pegs_given_all_wrong_pegs() {
+        List<Integer> expected = Arrays.asList(0, 0);
+
+        // TODO: expected call somewhere here...
+
+        List<Integer> result = Arrays.asList(1, 1);
+
+        assertEquals(expected, result);
     }
 }
