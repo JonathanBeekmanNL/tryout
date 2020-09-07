339ab8822e610b85f68c44a244fa6c33819f770c
Mike van Viegen <Mike.van.Viegen@rabobank.nl>
2020-09-07 11:33:36 +0200

    Setup first failing test

--- fizzbuzz/src/test/java/nl/rabobank/tryout/fizzbuzz/FizzbuzzTest.java
+++ fizzbuzz/src/test/java/nl/rabobank/tryout/fizzbuzz/FizzbuzzTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzbuzzTest {
     @Test
-    public void shouldAnswerWithTrue() {
+    public void something_should_be_tested_here() {
         assertEquals(true, true);
     }
 }
